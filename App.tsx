e5da85b343d41c2cc91f5ef3052f5f1c34debbb3
Vadim Savin <savinvadim1312@gmail.com>
2020-10-16 16:38:39 +0300

    implement auth

--- App.tsx
+++ App.tsx
@@ -6,11 +6,12 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { withAuthenticator } from 'aws-amplify-react-native'
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -25,3 +26,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App)
