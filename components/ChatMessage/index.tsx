31832ada145944764d9b66e4b5738cd493067739
Vadim Savin <savinvadim1312@gmail.com>
2020-10-16 19:24:12 +0300

    fetch and display messages

--- components/ChatMessage/index.tsx
+++ components/ChatMessage/index.tsx
@@ -6,13 +6,14 @@ import styles from './styles';
 
 export type ChatMessageProps = {
   message: Message;
+  myId: String,
 }
 
 const ChatMessage = (props: ChatMessageProps) => {
-  const { message } = props;
+  const { message, myId } = props;
 
   const isMyMessage = () => {
-    return message.user.id === 'u1';
+    return message.user.id === myId;
   }
 
   return (
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, Text, ImageBackground } from 'react-native';
 
 import { useRoute } from '@react-navigation/native';
+import {
+  API,
+  graphqlOperation,
+  Auth,
+} from 'aws-amplify';
 
-import chatRoomData from '../data/Chats';
+import { messagesByChatRoom } from '../src/graphql/queries';
+
 import ChatMessage from "../components/ChatMessage";
 import BG from '../assets/images/BG.png';
 import InputBox from "../components/InputBox";
 
 const ChatRoomScreen = () => {
 
+  const [messages, setMessages] = useState([]);
+  const [myId, setMyId] = useState(null);
+
   const route = useRoute();
+  console.log(route.params.id)
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messagesData = await API.graphql(
+        graphqlOperation(
+          messagesByChatRoom, {
+            chatRoomID: route.params.id,
+            sortDirection: "DESC",
+          }
+        )
+      )
+
+      setMessages(messagesData.data.messagesByChatRoom.items);
+    }
+    fetchMessages();
+  }, [])
+
+  useEffect(() => {
+    const getMyId = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser();
+      setMyId(userInfo.attributes.sub);
+    }
+    getMyId();
+  }, [])
 
   return (
     <ImageBackground style={{width: '100%', height: '100%'}} source={BG}>
       <FlatList
-        data={chatRoomData.messages}
-        renderItem={({ item }) => <ChatMessage message={item} />}
+        data={messages}
+        renderItem={({ item }) => <ChatMessage myId={myId} message={item} />}
         inverted
       />
 
